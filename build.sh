61a9df421acce1382a7996f9c85b2df92bec070c
Hotlands Software <weatherisland3@gmail.com>
2025-10-17 17:41:38 -0400

    fuck pyinstaller. i'm switching to AppImage

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-#/bin/bash
-pyinstaller --onefile --name "NotepadPypp" src/notepadpypp/main.py --hidden-import=PyQt6.Qsci
+#!/bin/bash
+
+PYQT6_PATH=$(python -c "import PyQt6; import os; print(os.path.dirname(PyQt6.__file__))")
+QSCI_PATH=$(python -c "import PyQt6.Qsci; import os; print(os.path.dirname(PyQt6.Qsci.__file__))")
+
+echo "PyQt6 path: $PYQT6_PATH"
+echo "QScintilla path: $QSCI_PATH"
+
+pyinstaller --name "NotepadPypp" \
+    --hidden-import=PyQt6.Qsci \
+    --collect-all PyQt6.Qsci \
+    src/notepadpypp/main.py
